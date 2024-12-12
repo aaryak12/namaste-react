db8e25b4ae7821c7266cd37afb93a1af73ce3587
Aaryak Kapoor <aaryakkapoor@gmail.com>
2024-12-12 13:48:19 +0530

    episode 3

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc" },
-  "Hello world from React 18.2.0!"
+const Title = () => (
+  <h1 className="heading" tabIndex={5}>
+    Namaste React using JSX
+  </h1>
+);
+
+const HeadingComponent = () => (
+  <div className="container">
+    <Title/>
+    <h1 className="heading">Namste React Functional Component</h1>
+  </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading); //takes the heading object and converts it to the <h1> tag which the browser can understand
+
+root.render(<HeadingComponent/>);
